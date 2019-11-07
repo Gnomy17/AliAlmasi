59e3d21d428e9b84be0e739c6bfa2203c3ee72d9
Gnomy17 <gnomy@ce.sharif.edu>
2019-11-07 18:11:45 +0330

    login form added

--- first/views.py
+++ first/views.py
@@ -1,11 +1,11 @@
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login as auth_login, authenticate
 from django.shortcuts import render, redirect
-from first.forms import SignUpForm
+from first.forms import SignUpForm, LoginForm
 
 
 # Create your views here.
 def base_html(request):
-    return render(request, 'base.html')
+    return render(request, 'base.html', {'user': request.user.is_authenticated})
 
 
 def register(request):
@@ -17,7 +17,25 @@ def register(request):
             raw_pass = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_pass)
             login(request, user)
-            return redirect('register')
+            return redirect('/register')
     else:
         form = SignUpForm()
     return render(request, 'register.html', {'form': form})
+
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user:
+            auth_login(request, user)
+            return redirect('/')
+        else:
+            error = "You suck!"
+            form = LoginForm()
+            # return redirect('/login', {'error': error})
+            return render(request, 'login.html', {'error': error, 'form': form})
+    else:
+        form = LoginForm()
+    return render(request, 'login.html', {'form': form})
