0cc1e3de8716f0b3d27ac716856c8d99a816e073
Gnomy17 <gnomy@ce.sharif.edu>
2019-11-07 12:38:09 +0330

    minor changes, urls changed

--- Webeloped/urls.py
+++ Webeloped/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from first.views import base_html
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', base_html)
 ]
