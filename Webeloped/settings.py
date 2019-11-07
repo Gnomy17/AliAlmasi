90d2ab647c6b11a792dd87aea285a9a73eec281a
ahmad <ahmadrahimiuni@gmail.com>
2019-11-07 16:39:46 +0330

    everything works

--- Webeloped/settings.py
+++ Webeloped/settings.py
@@ -119,3 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+)
